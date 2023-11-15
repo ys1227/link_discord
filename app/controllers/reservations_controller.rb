a82214e83baffd715a44c50cbcc6709f687c12d1
ys1227 <y.saki1227@outlook.jp>
2023-11-15 18:24:02 +0900

    削除リンクが動くように修正しました

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -10,14 +10,16 @@ class ReservationsController < ApplicationController
     @question = Question.find(params[:question_id])
     @reservations = Reservation.all || []
     @reservation = Reservation.new
+    @question_reservations = @question.reservations || []
   end
 
   def create
     @reservations = Reservation.all || []
     @question = Question.find(params[:question_id])
     @reservation = @question.reservations.build(reservations_params)
+    @question_reservations = @question.reservations || []
     if @reservation.save
-      redirect_to question_reservations_path, success: '募集時間が登録できました'
+      redirect_to new_question_reservation_path(@question), success: '募集時間が登録できました'
     else
       flash.now[:danger] ="投稿に失敗しました"
       render :new, status: :unprocessable_entity
@@ -25,17 +27,26 @@ class ReservationsController < ApplicationController
   end
 
   def update
-    
+    @question = Question.find(params[:question_id])
+    @reservations = @question.reservations
+
+    if @reservations.update(second_reservation_params)
+      redirect_to questions_path, success: '募集時間が登録できました'
+    else
+      flash.now[:danger] ="投稿に失敗しました"
+      render :index, status: :unprocessable_entity
+    end
   end
 
   def show
   end
 
   def destroy
+    @question = Question.find(params[:question_id])
     @reservation = Reservation.find(params[:id])
     @reservation.destroy!
 
-    redirect_to question_reservations_path, status: :see_other
+    redirect_to new_question_reservation_path(@question), status: :see_other
   end
   
   private
@@ -44,4 +55,7 @@ class ReservationsController < ApplicationController
     params.require(:reservation).permit(:rank, :start_time)
   end
 
+  def second_reservation_params
+    params.require(:reservation).permit(:rank)
+  end
 end
