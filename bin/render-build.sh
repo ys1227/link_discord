bcfe7330c2856f8f285d0bd8e88bcad3dae6a9b7
ys1227 <y.saki1227@outlook.jp>
2023-11-22 19:21:03 +0900

    fix:render_build.shの内容を変更

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,5 +6,5 @@ bundle install
 yarn install
 yarn build # jsファイルをesbuildでバンドルしているため
 bundle exec rails assets:clobber
-bundle exec rails assets:precompile # cssはsprocketsを使っているため
+bundle exec rails assets:precompile  # cssはsprocketsを使っているため
 bundle exec rails db:migrate # migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います）
