cb6701ca762bf10c5378da31aea4fe6c8c0a3e23
ys1227 <y.saki1227@outlook.jp>
2023-11-22 18:04:32 +0900

    デプロイのランタイムを変更

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,6 @@ set -o errexit
 bundle install
 yarn install
 yarn build # jsファイルをesbuildでバンドルしているため
-bundle exec rake assets:precompile # cssはsprocketsを使っているため
+bundle exec rails assets:clobber
+bundle exec rails assets:precompile # cssはsprocketsを使っているため
 bundle exec rails db:migrate # migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います）
