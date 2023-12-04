2b3675b14382f5c0824524c98c0ab3182e64ba7b
ys1227 <y.saki1227@outlook.jp>
2023-12-04 13:31:11 +0900

    add:job作成

---
+++ app/jobs/confirm_maching_date_job.rb
@@ -0,0 +1,27 @@
+class ConfirmMachingDateJob < ApplicationJob
+  queue_as :confirm_date
+
+  def perform(question)
+    reservations = []
+    question.reservations.each do |reservation|
+      voted_count = reservation.votes.count
+      rank_value = Reservation.ranks[reservation.rank]  # enumの値を数値に変換
+      combined_value = [reservation.id, rank_value, reservation.start_time, voted_count]
+      reservations << combined_value
+    end
+    sorted_reservations = reservations.sort_by { |id, rank, start_time, votes| [-votes, rank] }
+    if sorted_reservations.all? { |reservation| reservation[3] == 0 }
+      NotifyUnvotedQuestionOwnerJob.perform_later(question.user)
+    else
+      MatchingTime.create!(reservation_id:sorted_reservations[0][0],matching_data:sorted_reservations[0][2])
+      reservation = Reservation.find(sorted_reservations[0][0])
+      wait_time = 5
+      reservation.votes.each do |vote|
+        voted_user = vote.user
+        NotifyMostVotedTimeParticipantsJob.set(wait: wait_time.seconds).perform_later(reservation,voted_user)
+        wait_time += 5
+      end
+      NotifySelectedMeetingTimeToQuestionOwnerJob.set(wait: wait_time.seconds).perform_later(reservation,question.user)
+    end
+  end
+end
---
+++ test/jobs/chenge_question_state_job_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ChangeQuestionStateJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
