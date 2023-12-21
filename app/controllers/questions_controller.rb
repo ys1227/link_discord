6ae5c54b3663c4d2964cdc514d16d8deed35e66c
ys1227 <y.saki1227@outlook.jp>
2023-12-21 16:28:33 +0900

    add:レイアウトの修正

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,7 +7,7 @@ class QuestionsController < ApplicationController
 
   def index
     @q = Question.ransack(params[:q])
-    @questions = @q.result(distinct: true).includes(:user)#.where(state: "published")
+    @questions = @q.result(distinct: true).includes(:user).order(deadline: :desc)
   end
 
   def show
