8bd639a1945b36ea235c5a6c396adf8f7598dba4
ys1227 <y.saki1227@outlook.jp>
2024-02-15 21:58:53 +0900

    fix:構文を修正✨

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
-Rspec.describe 'Discordログイン', type: :system do 
+RSpec.describe 'Discordログイン', type: :system do 
   context 'ユーザーがDiscordでログインする' do
+    it '正常にログインできる' do
     visit root_path
     click_on 'Login with Discord'
 
     expect(page).to have_content('ログインに成功しました')
+    end
   end
 end
