9c6ebae1c3edaa0b640e526226440ec4d7676b10
ys1227 <y.saki1227@outlook.jp>
2024-03-16 21:28:38 +0900

    add:user_specの追加💄

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,11 @@ RSpec.describe 'User', type: :model do
       expect(user_without_email).to be_invalid
     end
 
+    it 'uidがない場合は無効' do
+      user_without_uid = FactoryBot.build(:user, uid: '')
+      expect(user_without_uid).to be_invalid
+    end
+
     it '重複したメールアドレスは無効' do
       user = FactoryBot.create(:user)
       user_deplicate_email = FactoryBot.build(:user, email: user.email)
@@ -27,11 +32,25 @@ RSpec.describe 'User', type: :model do
   describe 'ユーザーの関連付けに関するテスト' do
     before do
       @user = FactoryBot.create(:user)
+      @another_user = FactoryBot.create(:user)
     end
 
     it 'questionとの関連付けが正しく設定されていること' do
       question = FactoryBot.create(:question, user: @user)
       expect(@user.questions).to include question
+    end
+
+    it 'messageとの関連付けが正しく設定されていること' do
+      message = FactoryBot.create(:message, user: @user)
+      expect(@user.messages).to include message
+    end
+
+    it 'voteとの関連付けが正しく設定されていること' do
+      question = FactoryBot.create(:question, user:@user)
+      reservation = FactoryBot.create(:reservation, question: question)
+      vote = FactoryBot.create(:vote, user:@another_user, reservation: reservation)
+
+      expect(@another_user.votes).to include vote
     end
   end
 end
