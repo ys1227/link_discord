8b281363e5ae7b903f9a89ed017a74cf343421f3
ys1227 <y.saki1227@outlook.jp>
2024-03-17 19:32:06 +0900

    add:user_specの追加💄

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,13 @@ FactoryBot.define do
     sequence(:name) { |n| "user_#{n}" }
     sequence(:email) { |n| "user_#{n}@example.com" }
     sequence(:uid) { |n| "#{n}"}
+
+    trait :past do
+      created_at { DateTime.now.ago(1.hours) }
+    end
+
+    trait :future do
+      created_at { DateTime.now.since(1.hours) }
+    end
   end
 end
