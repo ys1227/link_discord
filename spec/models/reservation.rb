0ff5146c5474c1217db5495fe20a89ca1b859196
ys1227 <y.saki1227@outlook.jp>
2024-04-20 23:38:39 +0900

    add:reservationモデルのテスト追加✨

--- spec/models/reservation.rb
+++ spec/models/reservation.rb
@@ -28,4 +28,33 @@ RSpec.describe Question, type: :model do
       expect(@reservation.matching_time).to eq @matching_time
     end
   end
+
+  describe 'ユーザーのenum値に関するテスト' do
+    before do 
+      @reservation = FactoryBot.create(:reservation)
+    end
+    it 'enumで定義したrankの値がモデルに正しく設定されるか' do
+      expect(@reservation.rank).to eq 'default'
+    end
+
+    it 'rankに関するenumのメソッドが正しく定義されているか' do
+      expect(@reservation).to respond_to(:default?)
+      expect(@reservation).to respond_to(:one?)
+      expect(@reservation).to respond_to(:two?)
+      expect(@reservation).to respond_to(:three?)
+    end
+
+    it 'rankをメソッドを使用して正しく変更できるか' do
+      @reservation.default!
+      expect(@reservation.rank).to eq 'default'
+      @reservation.one!
+      expect(@reservation.rank).to eq 'one'
+      @reservation.two!
+      expect(@reservation.rank).to eq 'two'
+      @reservation.three!
+      expect(@reservation.rank).to eq 'three'
+      
+      
+    end
+  end
 end
