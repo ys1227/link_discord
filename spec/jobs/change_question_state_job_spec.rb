65c25f6a95095486ce15a4bee599488f22824c8b
ys1227 <y.saki1227@outlook.jp>
2024-05-08 22:55:14 +0900

    add:system_specの追加✨

--- spec/jobs/change_question_state_job_spec.rb
+++ spec/jobs/change_question_state_job_spec.rb
@@ -5,8 +5,7 @@ RSpec.describe ChangeQuestionStateJob, type: :worker do
   let!(:question1) { FactoryBot.create(:question, state: 'published', deadline: 1.day.ago) }
   let!(:question2) { FactoryBot.create(:question, state: 'published', deadline: 1.day.ago) }
 
-    it '現在時刻より前に投稿されたquestionがjobの実行によってstateカラムを正しい状態に変化させるか' do
-
+    it '現在時刻より前に投稿されたquestionのstateカラムを正しい状態に変化させるか' do
       described_class.perform_now
       expect(question1.reload.state).to eq("closed")
       expect(question2.reload.state).to eq("closed")
---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe 'Tasks', type: :system do
+  let(:question) { FactoryBot.create(:question) }
+
+  describe 'ログイン前' do
+    describe 'ページ遷移確認' do
+      context '投稿作成画面にアクセス' do
+        it '正常にアクセスができる' do
+          visit new_question_path
+          expect(current_path).to eq new_question_path
+        end
+      end
+
+      context '投稿一覧画面にアクセス' do
+        it '正常にアクセスができる' do
+          visit questions_path
+          expect(current_path).to eq questions_path
+        end
+      end
+
+      context '投稿一覧画面にアクセス' do
+        it '正常にアクセスができる' do
+          visit questions_path
+          expect(current_path).to eq questions_path
+        end
+      end
+
+      context '質問詳細画面にアクセス' do
+        it '正常に質問詳細画面にアクセスができる' do
+          visit question_path(question)
+          expect(current_path).to eq question_path(question)
+        end
+      end
+    end
+  end
+end
